226fa5bf990597bda750b42c47f589f66a6b0783
mechaskrom <mechaskrom@gmail.com>
2022-04-20 20:39:00 +0200

    First commit

---
+++ GifWriter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+namespace Mechaskrom.Gif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a palette.
+            Color[] palette = new Color[256];
+            palette[0] = Color.Red; //Index 0 = red.
+            palette[1] = Color.Green; //Index 1 = green.
+            palette[2] = Color.Blue; //Index 2 = blue.
+
+            //Create a few frames. Frames can also be constructed from 8-bit bitmaps.
+            GifFrame frame1 = new GifFrame(32, 32); //Width, height.
+            for (int i = 0; i < frame1.Length; i++)
+            {
+                frame1.Pixels[i] = 0; //Set all pixels to index 0 = red.
+            }
+            GifFrame frame2 = new GifFrame(32, 32);
+            for (int i = 0; i < frame2.Length; i++)
+            {
+                frame2.Pixels[i] = 1; //Set all pixels to index 1 = green.
+            }
+            GifFrame frame3 = new GifFrame(32, 32);
+            for (int i = 0; i < frame3.Length; i++)
+            {
+                frame3.Pixels[i] = 2; //Set all pixels to index 2 = blue.
+            }
+
+            //Create gif writer and add the frames to it.
+            GifWriter gifWriter = new GifWriter(palette, 0); //Global palette, loop count.
+            gifWriter.addFrame(frame1, null, 50); //Frame, local palette, display delay between frames.
+            gifWriter.addFrame(frame2, null, 50);
+            gifWriter.addFrame(frame3, null, 50);
+
+            //Save result as a gif file.
+            gifWriter.save(Path.Combine(Environment.CurrentDirectory, "result.gif"));
+        }
+    }
+}
---
+++ GifWriter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GifWriter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GifWriter")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1dfa925d-d468-4988-aea4-0b25e3776c61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
